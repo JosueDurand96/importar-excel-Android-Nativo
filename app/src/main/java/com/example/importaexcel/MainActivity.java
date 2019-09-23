213a4a2073b80be140ddfd721eb1fd71fe50479f
JosueDurand96 <josue.durand8@gmail.com>
2019-09-23 17:37:05 -0500

    terminado

--- app/src/main/java/com/example/importaexcel/MainActivity.java
+++ app/src/main/java/com/example/importaexcel/MainActivity.java
@@ -2,13 +2,87 @@ package com.example.importaexcel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.InputStream;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG ="main";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        Button button =(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readExcelFileFromAssets();
+            }
+        });
+
+
     }
+
+    public void readExcelFileFromAssets() {
+        try {
+            InputStream myInput;
+            // initialize asset manager
+            AssetManager assetManager = getAssets();
+            //  open excel sheet
+            myInput = assetManager.open("myData.xls");
+            // Create a POI File System object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            // We now need something to iterate through the cells.
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            int rowno =0;
+            textView.append("\n");
+            while (rowIter.hasNext()) {
+                Log.e(TAG, " row no "+ rowno );
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                if(rowno !=0) {
+                    Iterator<Cell> cellIter = myRow.cellIterator();
+                    int colno =0;
+                    String sno="", date="", det="";
+                    while (cellIter.hasNext()) {
+                        HSSFCell myCell = (HSSFCell) cellIter.next();
+                        if (colno==0){
+                            sno = myCell.toString();
+                        }else if (colno==1){
+                            date = myCell.toString();
+                        }else if (colno==2){
+                            det = myCell.toString();
+                        }
+                        colno++;
+                        Log.e(TAG, " Index :" + myCell.getColumnIndex() + " -- " + myCell.toString());
+                    }
+                    textView.append( sno + " -- "+ date+ "  -- "+ det+"\n");
+                }
+                rowno++;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "error "+ e.toString());
+        }
+    }
+
 }
